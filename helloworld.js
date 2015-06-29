fae47cc755304624179742be879a2a916bdaa0c7
shita-aki <hiro.dev.release14@gmail.com>
2015-06-29 23:52:26 +0900

    リクエストURLをコンソールに表示。 あとポートを変更(ダミーだけど)。

--- helloworld.js
+++ helloworld.js
@@ -4,12 +4,13 @@ var myHttp = require('http');					// http関連のモジュールを読み込む
 
 var myServer = myHttp.createServer();			// webサーバオブジェクトを作る
 myServer.on('request', httpPrint);				// requestが来たらhttpPrintを実行するように設定
-myServer.listen(443);							// 443ポートで待ち受け
-console.log('Server running at MY_SERVER:443/');// "443で待ってるぜ"とコンソールに表示
+myServer.listen(1024);							// 指定ポートで待ち受け
+console.log('Server running at MY_SERVER.');	// "MY_SERVERで待ってるぜ"とコンソールに表示
 
 function httpPrint (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
 										// "コンテントタイプはテキストだ"と送信
 										// 200はhttpのstatus codeで、「要求を正常に終了した」を表すもののようだ
   response.end('Hello World !\n');		// "Hello world !"を送信
+  console.log(request.url);				// リクエストURLをコンソールに表示
 }
